5e953e0a71b9e6265587721e75143cfe629c703c
naman3309 <nggoel888@gmail.com>
2023-10-05 21:56:08 +0530

    Adding a Cart Page

---
+++ lib/Pages/Cart_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: "Cart".text.bold.xl2.make(),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      
+    );
+  }
+}
